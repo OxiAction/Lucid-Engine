9ed31965ef293936e1611814d8def003e5fe5448
Michael Schreiber <schreiber.gausburg@web.de>
2017-12-21 17:25:52 +0100

    restructured the whole project files for better seperation between the Engine itself and demo projects

--- files/tilesets/tileset1.ts
+++
@@ -1,7 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<ts>
-	<tileset id="tileset1" name="Tileset 1" />
-	<tiles>
-		<tile id="gras" name="Gras" x="0" y="0" width="20" height="20" />
-	</tiles>
-</ts>
